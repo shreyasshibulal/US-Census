38c7e3d826585dd05c40ae0aca41056acddabf7e
adamvan101 <avanaken92@gmail.com>
2014-11-25 20:12:57 -0500

    Removed up/down migration

--- db/migrate/20141121010651_create_data.rb
+++ db/migrate/20141121010651_create_data.rb
@@ -2,21 +2,9 @@ class CreateData < ActiveRecord::Migration
   def change
     create_table :data do |t|
       t.string :key
-      t.string :value
+      t.text :value
 
       t.timestamps
-    change_column :data, :value, :text
-    end
-  end
-
-  def self.up
-    change_table :tablename do |t|
-      t.change :value, :text
-    end
-  end
-  def self.down
-    change_table :tablename do |t|
-      t.change :value, :text
     end
   end
 end
