370197e32220ab8594b2fc21c93c35ce5e26c235
adamvan101 <avanaken92@gmail.com>
2014-11-25 19:17:40 -0500

    Removed migration

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,12 +6,12 @@
 require 'xmlsimple'
 require 'open-uri'
 
-class CreateProducts < ActiveRecord::Migration
-  def change
-      change_column :data, :value, :text
-    end
-  end
-end
+# class CreateProducts < ActiveRecord::Migration
+#   def change
+#       change_column :data, :value, :text
+#     end
+#   end
+# end
 
 class MainController < ApplicationController
   def index
